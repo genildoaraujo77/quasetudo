b2c71204201bf978205f08fae8a6a0a8786d8107
genildoaraujo77 <genildo.araujo77@outlook.com>
2021-01-16 13:35:14 -0300

    nova chamada para home

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,8 @@ import React, { useState } from 'react';
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Bem vindo ao site QuaseTudo</h2>
       <Contador />
-    </div>
-  )
-}
-
-const Contador = () => {
-  const [contador, setContador] = useState(1);
-  
-  function adicionarContador() {
-    setContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>Adicionar</button>
     </div>
   )
 }
