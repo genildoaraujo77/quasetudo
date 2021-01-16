b5093c0505613bd816b9e9acd691b68e02b06aa8
genildoaraujo77 <genildo.araujo77@outlook.com>
2021-01-16 13:46:30 -0300

    retirando erro de variavel contador não definida

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <h2>Bem vindo ao site QuaseTudo - Dev. FullStack Genildo Araujo</h2>
-      <Contador />
     </div>
   )
 }
